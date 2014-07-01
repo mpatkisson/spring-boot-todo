abff0d6e8de003f340bc317c3ddccd458eb77290
Mike Atkisson <devnull@localhost>
2014-07-01 06:26:02 -0400

    Added new entity and began using it in controller.

---
+++ src/main/java/com/triptacular/core/Task.java
@@ -0,0 +1,36 @@
+package com.triptacular.core;
+
+public class Task {
+    
+    private int id;
+    private String item;
+
+    /**
+     * @return the id
+     */
+    public int getId() {
+        return id;
+    }
+
+    /**
+     * @param id the id to set
+     */
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    /**
+     * @return the item
+     */
+    public String getItem() {
+        return item;
+    }
+
+    /**
+     * @param item the item to set
+     */
+    public void setItem(String item) {
+        this.item = item;
+    }
+    
+}
--- src/main/java/com/triptacular/web/TaskController.java
+++ src/main/java/com/triptacular/web/TaskController.java
@@ -1,18 +1,26 @@
 package com.triptacular.web;
 
+import com.triptacular.core.Task;
+import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Controller
+@RestController
 public class TaskController {
 
-    @RequestMapping("/tasks")
-    String index() {
+    private List<Task> tasks;
+    
+    @RequestMapping("/tasks/index")
+    public String index() {
         return "tasks";
     }
 
-    // GET all tasks
-    // @RequestMapping("/tasks")
+    @RequestMapping("/tasks")
+    public List<Task> getAll() {
+        return tasks;
+    }
 
     // GET task by ID
     // @RequestMapping("/tasks/task/{id}")
