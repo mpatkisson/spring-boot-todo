79f5b1d27202a315ea8d22c091bd4b08398eccaa
Michael Atkisson <mpatkisson@gmail.com>
2015-03-16 10:42:14 -0400

    Improved Unit Tests

--- src/test/java/com/triptacular/web/TaskControllerIntegrationTest.java
+++ src/test/java/com/triptacular/web/TaskControllerIntegrationTest.java
@@ -2,22 +2,28 @@ package com.triptacular.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.triptacular.Application;
+import com.triptacular.core.Task;
 import com.triptacular.services.TaskService;
 import org.jongo.MongoCollection;
 import org.junit.After;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import javax.servlet.ServletContext;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
@@ -29,6 +35,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standal
 public class TaskControllerIntegrationTest extends ControllerIntegrationTest {
 
     private static final String VIEW = "tasks";
+    private static final String REDIRECT_VIEW = "redirect:/tasks";
     private static final String FORWARDED_URL = "/templates/tasks.html";
     private static final String JSON_CONTENT_TYPE = "application/json;charset=UTF-8";
     private ObjectMapper mapper;
@@ -65,5 +72,29 @@ public class TaskControllerIntegrationTest extends ControllerIntegrationTest {
         .andExpect(view().name(VIEW))
         .andExpect(forwardedUrl(FORWARDED_URL));
     }
-    
+
+    @Test
+    public void canCreate () throws Exception {
+        Assert.assertTrue(service.getCount() == 0);
+        Task task = new Task();
+        task.setItem("Do This");
+        MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.post("/tasks/create", task)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+        mockMvc.perform(builder);
+        Assert.assertTrue(service.getCount() == 1);
+    }
+
+    @Test
+    public void createRedirectsToIndex() throws Exception {
+        Task task = new Task();
+        task.setItem("Do This");
+        MockHttpServletRequestBuilder builder =
+                MockMvcRequestBuilders.post("/tasks/create", task)
+                        .contentType(MediaType.APPLICATION_FORM_URLENCODED);
+        mockMvc.perform(builder)
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(view().name(REDIRECT_VIEW));
+    }
 }
